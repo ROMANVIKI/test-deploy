6487efdf2c98fe01b17f35a3ba6751e255b918db
ROMANVIKI <110844122+ROMANVIKI@users.noreply.github.com>
2023-03-23 16:21:00 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-24jxhiy!z6un9)!!sf(ibs$!87d%@qc6%x#gi35(294env#1!4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
